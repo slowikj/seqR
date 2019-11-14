02583da426e9cb9227db038b2d3b41b78ccb1b72
slowikj <slowikj@student.mini.pw.edu.pl>
2019-11-14 10:54:47 +0100

    count_kmers: add test

--- tests/testthat/test_count_kmers.R
+++ tests/testthat/test_count_kmers.R
@@ -10,3 +10,14 @@ test_that("count positional unigrams", {
   expected <- c("0_c"=1, "2_a"=1, "1_b"=2, "0_a"=1, "2_c"=1)
   expect_mapequal(res, expected)
 })
+
+test_that("count non positional unigrams", {
+  res <- count_kmers(
+    matrix(data=c("a", "c", "b", "b", "a", "c"), nrow=2),
+    c(),
+    c("a", "b", "c"),
+    pos=FALSE)
+  
+  expected <- c("a"=2, "b"=2, "c"=2)
+  expect_mapequal(res, expected)
+})
