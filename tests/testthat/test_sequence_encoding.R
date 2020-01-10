1cd3225a2398f2857a0853a9ed8ca32098e09aad
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-01-11 00:52:04 +0100

    test_sequence_encoding: refactor
    Move common logic to a separate function: invoke_string_sequence_test

--- tests/testthat/test_sequence_encoding.R
+++ tests/testthat/test_sequence_encoding.R
@@ -1,21 +1,24 @@
 library(testthat)
 
+invoke_string_sequence_test <- function(sequence, alphabet, expected_res) {
+  res <- seqR::enumerate_string_sequence(sequence=sequence, alphabet=alphabet)
+  expect_equal(res, expected_res)
+}
+
 test_that("enumerate string vector", {
-  res <- seqR::enumerate_string_sequence(sequence=c("a", "a", "bb", "aa", "b"),
-                                         alphabet=c("a", "aa"))
-  expect_equal(res, c(1, 1, -1, 2, -1))
+  invoke_string_sequence_test(sequence=c("a", "a", "bb", "aa", "b"),
+                              alphabet=c("a", "aa"),
+                              expected_res=c(1, 1, -1, 2, -1))
 })
 
 test_that("return a vector filled with -1 for an empty (null) alphabet", {
-  res <- seqR::enumerate_string_sequence(sequence=c("a", "b", "c"),
-                                         alphabet=c())
-  
-  expect_equal(res, rep(-1, 3))
+  invoke_string_sequence_test(sequence=c("a", "b", "c"),
+                              alphabet=c(),
+                              expected_res=rep(-1, 3))
 })
 
 test_that("return an empty vector if sequence is empty (null)", {
-  res <- seqR::enumerate_string_sequence(sequence=c(),
-                                         alphabet=c("a"))
-  
-  expect_equal(res, integer(0))
+  invoke_string_sequence_test(sequence=c(),
+                              alphabet=c("a"),
+                              expected_res=integer(0))
 })
