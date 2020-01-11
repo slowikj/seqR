67474c55a14f5247d87ecd75a3c3cb8f5004252a
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-01-11 03:35:16 +0100

    sequence_integer_coding: refactor: change vars' names + extract typedef
    typedef: ITEM_ENCODING_TYPE
    
    Change val2short... -> val2num...
    
    Change short2val... num2val...

--- src/sequence_integer_coding.cpp
+++ src/sequence_integer_coding.cpp
@@ -7,51 +7,53 @@
 #include<functional>
 #include<tuple>
 
+typedef short ITEM_ENCODING_TYPE;
+
 template<class ALPHABET_VECTOR_TYPE, class CPP_ITEM_TYPE, class RCPP_ITEM_TYPE>
-std::tuple<std::unordered_map<CPP_ITEM_TYPE, short>*, std::unordered_map<short, CPP_ITEM_TYPE>*>
+std::tuple<std::unordered_map<CPP_ITEM_TYPE, ITEM_ENCODING_TYPE>*, std::unordered_map<ITEM_ENCODING_TYPE, CPP_ITEM_TYPE>*>
 enumerate_alphabet(ALPHABET_VECTOR_TYPE& alphabet,
                    std::function<CPP_ITEM_TYPE(const RCPP_ITEM_TYPE&)> rcpp2cpp_converter) {
-  short least_available_number = 1;
-  auto val2short_encoder = new std::unordered_map<CPP_ITEM_TYPE, short>();
-  auto short2val_decoder = new std::unordered_map<short, CPP_ITEM_TYPE>();
+  ITEM_ENCODING_TYPE least_available_number = 1;
+  auto val2num_encoder = new std::unordered_map<CPP_ITEM_TYPE, ITEM_ENCODING_TYPE>();
+  auto num2val_decoder = new std::unordered_map<ITEM_ENCODING_TYPE, CPP_ITEM_TYPE>();
   for(const auto& alphabet_item: alphabet) {
     CPP_ITEM_TYPE cpp_alfabet_item = rcpp2cpp_converter(alphabet_item);
-    if(val2short_encoder->find(cpp_alfabet_item) == val2short_encoder->end()) {
-      (*val2short_encoder)[cpp_alfabet_item] = least_available_number;
-      (*short2val_decoder)[least_available_number] = cpp_alfabet_item;
+    if(val2num_encoder->find(cpp_alfabet_item) == val2num_encoder->end()) {
+      (*val2num_encoder)[cpp_alfabet_item] = least_available_number;
+      (*num2val_decoder)[least_available_number] = cpp_alfabet_item;
       ++least_available_number;
     }
   }
-  return {val2short_encoder, short2val_decoder};
+  return {val2num_encoder, num2val_decoder};
 }
 
 const short NOT_ALLOWED_CHARACTER_CODE = -1;
 
 template<class VECTOR_TYPE, class CPP_ITEM_TYPE, class RCPP_ITEM_TYPE>
 std::vector<short> enumerate_sequence(VECTOR_TYPE& seq,
-                                      std::unordered_map<CPP_ITEM_TYPE, short>* val2short_encoder,
+                                      std::unordered_map<CPP_ITEM_TYPE, ITEM_ENCODING_TYPE>* val2num_encoder,
                                       std::function<CPP_ITEM_TYPE(const RCPP_ITEM_TYPE&)> rcpp2cpp_converter) {
   std::vector<short> res;
   res.reserve(seq.size());
   for(const auto& seq_item: seq) {
     CPP_ITEM_TYPE item_cpp = rcpp2cpp_converter(seq_item);
-    short item_encoded = val2short_encoder -> find(item_cpp) == val2short_encoder -> end()
+    short item_encoded = val2num_encoder -> find(item_cpp) == val2num_encoder -> end()
       ? NOT_ALLOWED_CHARACTER_CODE
-      : (*val2short_encoder)[item_cpp];
+      : (*val2num_encoder)[item_cpp];
     res.push_back(item_encoded);
   }
   return res;
 }
 
 template<class VECTOR_TYPE, class CPP_ITEM_TYPE, class RCPP_ITEM_TYPE>
-std::tuple<std::vector<short>, std::unordered_map<CPP_ITEM_TYPE, short>*, std::unordered_map<short, CPP_ITEM_TYPE>*>
+std::tuple<std::vector<short>, std::unordered_map<CPP_ITEM_TYPE, ITEM_ENCODING_TYPE>*, std::unordered_map<short, CPP_ITEM_TYPE>*>
 enumerate_sequence_nonnull(VECTOR_TYPE& sequence,
                            VECTOR_TYPE& alphabet,
                            std::function<CPP_ITEM_TYPE(const RCPP_ITEM_TYPE&)> rcpp2cpp_converter) {
-  auto [val2short_encoder, short2val_decoder] = enumerate_alphabet<VECTOR_TYPE, CPP_ITEM_TYPE, RCPP_ITEM_TYPE>(
+  auto [val2num_encoder, num2val_decoder] = enumerate_alphabet<VECTOR_TYPE, CPP_ITEM_TYPE, RCPP_ITEM_TYPE>(
     alphabet, rcpp2cpp_converter);
-  auto res = enumerate_sequence<VECTOR_TYPE, CPP_ITEM_TYPE, RCPP_ITEM_TYPE>(sequence, val2short_encoder, rcpp2cpp_converter);
-  return { res, val2short_encoder, short2val_decoder };
+  auto res = enumerate_sequence<VECTOR_TYPE, CPP_ITEM_TYPE, RCPP_ITEM_TYPE>(sequence, val2num_encoder, rcpp2cpp_converter);
+  return { res, val2num_encoder, num2val_decoder };
 }
 
 template<class NON_NULL_TYPE>
@@ -60,7 +62,10 @@ NON_NULL_TYPE get_value_or_construct_default(Rcpp::Nullable<NON_NULL_TYPE> nulla
 }
 
 template<class VECTOR_TYPE, class CPP_ITEM_TYPE, class RCPP_ITEM_TYPE>
-std::tuple<std::vector<short>, std::unordered_map<CPP_ITEM_TYPE, short>*, std::unordered_map<short, CPP_ITEM_TYPE>*>
+std::tuple<std::vector<ITEM_ENCODING_TYPE>,
+           std::unordered_map<CPP_ITEM_TYPE, ITEM_ENCODING_TYPE>*,
+           std::unordered_map<ITEM_ENCODING_TYPE, CPP_ITEM_TYPE>*
+>
 enumerate_sequence(Rcpp::Nullable<VECTOR_TYPE> sequence,
                    Rcpp::Nullable<VECTOR_TYPE> alphabet,
                    std::function<CPP_ITEM_TYPE(const RCPP_ITEM_TYPE&)> rcpp2cpp_converter) {
@@ -77,13 +82,13 @@ template<class VECTOR_TYPE, class CPP_ITEM_TYPE, class RCPP_ITEM_TYPE>
 Rcpp::IntegerVector enumerate_sequence_and_wrap(Rcpp::Nullable<VECTOR_TYPE> sequence,
                                                 Rcpp::Nullable<VECTOR_TYPE> alphabet,
                                                 std::function<CPP_ITEM_TYPE(const RCPP_ITEM_TYPE&)> rcpp2cpp_converter) {
-  auto [res, val2short_encoder, short2val_decoder] = enumerate_sequence<VECTOR_TYPE, CPP_ITEM_TYPE, RCPP_ITEM_TYPE>(
+  auto [res, val2num_encoder, num2val_decoder] = enumerate_sequence<VECTOR_TYPE, CPP_ITEM_TYPE, RCPP_ITEM_TYPE>(
     sequence,
     alphabet,
     rcpp2cpp_converter
   );
-  delete val2short_encoder;
-  delete short2val_decoder;
+  delete val2num_encoder;
+  delete num2val_decoder;
   return Rcpp::wrap(res);
 }
 
