8e1abc25c385cb80e3f18586f8e3623cc1f04910
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-01-12 15:53:53 +0100

    allowed_ranges_computation: add a test

--- tests/testthat/test_allowed_ranges_computation.R
+++ tests/testthat/test_allowed_ranges_computation.R
@@ -15,19 +15,25 @@ test_that("a sequence with all allowed characters and 3 window size gives one ra
 })
 
 test_that("a sequence (1,2,-1,3,4) and 1 window size gives 2 ranges", {
-  invoke_test(encoded_sequence= c(1,2,-1,4,5),
+  invoke_test(encoded_sequence=c(1,2,-1,4,5),
               window_length=1,
               expected_result=list(begin=c(1, 4), end=c(2, 5)))
 })
 
 test_that("a sequence (1,2,-1,3,4) and 3 window gives NO range", {
-  invoke_test(encoded_sequence= c(1,2,-1,3,4),
+  invoke_test(encoded_sequence=c(1,2,-1,3,4),
               window_length=3,
               expected_result=list(begin=integer(0), end=integer(0)))
 })
 
 test_that("a sequence (1,2,2,-1,1,1,-1,-1,-1,1) and 3 window gives only [1,1] range", {
-  invoke_test(encoded_sequence= c(1,2,2,-1,1,1,-1,-1,-1,1),
+  invoke_test(encoded_sequence=c(1,2,2,-1,1,1,-1,-1,-1,1),
               window_length=3,
               expected_result=list(begin=c(1), end=c(1)))
+})
+
+test_that("a sequence (1,1,1,1,1,1) with 100 window gives NO range", {
+  invoke_test(encoded_sequence=c(1,1,1,1,1,1),
+              window_length=100,
+              expected_result=list(begin=integer(0), end=integer(0)))
 })
