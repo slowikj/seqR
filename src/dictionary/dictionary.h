6fec947f5554c9ec49e6708c652df487b0990ddd
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-02-20 01:16:18 +0100

    Add dictionary interface +

---
+++ src/dictionary/dictionary.h
@@ -0,0 +1,18 @@
+#ifndef FIRST_DICTIONARY_H
+#define FIRST_DICTIONARY_H
+
+#include<vector>
+
+
+template<class K, class V>
+class Dictionary {
+public:
+  virtual V &operator[](const K &key) = 0;
+  
+  [[nodiscard]] virtual std::vector<int> get_keys() const = 0;
+  
+  virtual bool is_present(const K &key) const = 0;
+};
+
+
+#endif //FIRST_DICTIONARY_H
