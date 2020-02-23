cae668ca22b76450a42562c0d55c6f5ebf93c92d
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-02-23 12:21:06 +0100

    alphabet_encoder: add inline and const

--- src/alphabet_encoder/alphabet_encoder.h
+++ src/alphabet_encoder/alphabet_encoder.h
@@ -12,7 +12,7 @@ public:
     : input2internal_item_converter(input2internal_item_converter) { }
   
   template<class encoded_item_t, class input_t>
-  std::unique_ptr<Dictionary<internal_elem_t, encoded_item_t>> get_encoder(const input_t& input);
+  std::unique_ptr<Dictionary<internal_elem_t, encoded_item_t>> get_encoder(const input_t& input) const;
   
 private:
   const std::function<internal_elem_t(input_elem_t)> &input2internal_item_converter;
@@ -21,19 +21,20 @@ private:
 
 template<class input_elem_t, class internal_elem_t>
 template<class encoded_item_t, class input_t>
-std::unique_ptr<Dictionary<internal_elem_t, encoded_item_t>>
-AlphabetEncoder<input_elem_t, internal_elem_t>::get_encoder(const input_t &input) {
-  int currentNum = 1;
-  auto res = std::unique_ptr<Dictionary<internal_elem_t, encoded_item_t>>(
-    new Dictionary<internal_elem_t, encoded_item_t>());
-  for(const input_elem_t& inputElem: input) {
-    internal_elem_t internalElem = this->input2internal_item_converter(inputElem);
-    if(!res->isPresent(internalElem)) {
-      (*res)[internalElem] = currentNum++;
+inline
+  std::unique_ptr<Dictionary<internal_elem_t, encoded_item_t>>
+    AlphabetEncoder<input_elem_t, internal_elem_t>::get_encoder(const input_t &input) const {
+      int currentNum = 1;
+      auto res = std::unique_ptr<Dictionary<internal_elem_t, encoded_item_t>>(
+        new Dictionary<internal_elem_t, encoded_item_t>());
+      for(const input_elem_t& inputElem: input) {
+        internal_elem_t internalElem = this->input2internal_item_converter(inputElem);
+        if(!res->isPresent(internalElem)) {
+          (*res)[internalElem] = currentNum++;
+        }
+      }
+      return res;
     }
-  }
-  return res;
-}
 
 
 #endif //ALPHABET_ENCODER_H
