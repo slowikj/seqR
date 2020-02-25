7612a830fa748bf17547ad49cd21114e2ada2cee
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-02-25 01:13:16 +0100

    alphabet_encoder: change member field name

--- src/alphabet_encoder/alphabet_encoder.h
+++ src/alphabet_encoder/alphabet_encoder.h
@@ -8,14 +8,14 @@
 template<class input_elem_t, class internal_elem_t>
 class AlphabetEncoder {
 public:
-  explicit AlphabetEncoder(std::function<internal_elem_t(input_elem_t)> input2internal_item_converter)
-    : input2internal_item_converter(input2internal_item_converter) { }
+  explicit AlphabetEncoder(std::function<internal_elem_t(input_elem_t)> input_to_internal_item_converter)
+    : input_to_internal_item_converter(input_to_internal_item_converter) { }
   
   template<class encoded_item_t, class input_t>
   Dictionary<internal_elem_t, encoded_item_t> getEncoding(const input_t& input);
   
 private:
-  std::function<internal_elem_t(input_elem_t)> input2internal_item_converter;
+  std::function<internal_elem_t(input_elem_t)> input_to_internal_item_converter;
   
 };
 
@@ -26,7 +26,7 @@ Dictionary<internal_elem_t, encoded_item_t>
       encoded_item_t currentNum = 1;
       auto res = Dictionary<internal_elem_t, encoded_item_t>();
       for(const input_elem_t& inputElem: input) {
-        internal_elem_t internalElem = this->input2internal_item_converter(inputElem);
+        internal_elem_t internalElem = this->input_to_internal_item_converter(inputElem);
         if(!res.isPresent(internalElem)) {
           res[internalElem] = currentNum++;
         }
