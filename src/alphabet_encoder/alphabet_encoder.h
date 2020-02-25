73f0a8ebb5023834bfe75d932cd9a1f692077866
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-02-25 01:15:58 +0100

    alphabet_encoder: add move semantics for a constructor

--- src/alphabet_encoder/alphabet_encoder.h
+++ src/alphabet_encoder/alphabet_encoder.h
@@ -8,8 +8,8 @@
 template<class input_elem_t, class internal_elem_t>
 class AlphabetEncoder {
 public:
-  explicit AlphabetEncoder(std::function<internal_elem_t(input_elem_t)> input_to_internal_item_converter)
-    : input_to_internal_item_converter(input_to_internal_item_converter) { }
+  explicit AlphabetEncoder(std::function<internal_elem_t(input_elem_t)>&& input_to_internal_item_converter)
+    : input_to_internal_item_converter(std::move(input_to_internal_item_converter)) { }
   
   template<class encoded_item_t, class input_t>
   Dictionary<internal_elem_t, encoded_item_t> getEncoding(const input_t& input);
