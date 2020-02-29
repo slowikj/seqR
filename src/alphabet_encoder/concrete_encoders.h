2e23886810d0d4c14c14b7ccf880b20a33004409
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-02-29 22:04:56 +0100

    alphabet_encoder: refactor
    move common integer encoding logic to a helper function

--- src/alphabet_encoder/concrete_encoders.h
+++ src/alphabet_encoder/concrete_encoders.h
@@ -1,11 +1,21 @@
 #ifndef CONCRETE_ENCODERS_H
 #define CONCRETE_ENCODERS_H
 
+// [[Rcpp::plugins("c++17")]]
+#include <Rcpp.h>
+
 #include "alphabet_encoder.h"
+#include "../dictionary.h"
 #include <memory>
 
+// INTEGER ENCODING
 AlphabetEncoder<int, int> getIntegerAlphabetEncoder() {
   return AlphabetEncoder<int, int>([](const int& elem) -> int { return elem; });
+}
+
+Dictionary<int, short> getEncoding(const Rcpp::IntegerVector& input) {
+  auto encoder = getIntegerAlphabetEncoder();
+  return encoder.getEncoding<short, Rcpp::IntegerVector>(input);
 }
 
 #endif //CONCRETE_ENCODERS_H
--- src/alphabet_encoder_proxy.cpp
+++ src/alphabet_encoder_proxy.cpp
@@ -3,22 +3,17 @@
 #include<iostream>
 
 #include "alphabet_encoder/concrete_encoders.h"
-#include "alphabet_encoder/alphabet_encoder.h"
 #include <utility>
 
 //' @export
 // [[Rcpp::export]]
 Rcpp::IntegerVector encode_alphabet(Rcpp::IntegerVector input) {
-  AlphabetEncoder<int,int> integer_encoder = getIntegerAlphabetEncoder();
-  Dictionary<int, short> encoded = integer_encoder.getEncoding<short, Rcpp::IntegerVector>(input);
-  
+  Dictionary<int, short> encoded = getEncoding(input);
   auto res = Rcpp::IntegerVector(encoded.size());
   auto keys = encoded.getKeys();
-  
   for(int i = 0; i < keys.size(); ++i) {
     res[i] = encoded[keys[i]];
   }
   res.names() = keys;
-  
   return res;
 }
