ee50e3b92a65f21866c9d73dac0055dc889469a8
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-03-01 20:57:33 +0100

    Change typedef name: ENCODED_T -> ENCODED_ELEM_T

--- src/alphabet_encoder/concrete_encoders.h
+++ src/alphabet_encoder/concrete_encoders.h
@@ -8,24 +8,24 @@
 #include <memory>
 #include <string>
 
-typedef short ENCODED_T;
+typedef short ENCODED_ELEM_T;
 
 auto getEncoding(Rcpp::IntegerVector& input) {
-  return getAlphabetEncoding<Rcpp::IntegerVector, int, int, ENCODED_T>(
+  return getAlphabetEncoding<Rcpp::IntegerVector, int, int, ENCODED_ELEM_T>(
     input,
     [](const int& elem) -> int { return elem; }
   );
 }
   
 auto getEncoding(Rcpp::NumericVector& input) {
-  return getAlphabetEncoding<Rcpp::NumericVector, double, double, ENCODED_T>(
+  return getAlphabetEncoding<Rcpp::NumericVector, double, double, ENCODED_ELEM_T>(
     input,
     [](const double& elem) -> double { return elem; }
   );
 }
 
 auto getEncoding(Rcpp::StringVector& input) {
-  return getAlphabetEncoding<Rcpp::StringVector, Rcpp::String::StringProxy, std::string, ENCODED_T>(
+  return getAlphabetEncoding<Rcpp::StringVector, Rcpp::String::StringProxy, std::string, ENCODED_ELEM_T>(
     input,
     [](const Rcpp::String::StringProxy& elem) -> std::string { return Rcpp::as<std::string>(elem); }
   );
