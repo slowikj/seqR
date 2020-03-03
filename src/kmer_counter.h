ee0ae233044ab6e96243f19cf3560470eab6a77f
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-03-03 17:21:31 +0100

    kmer_counter: handle corner case - the last window in the sequence

--- src/kmer_counter.h
+++ src/kmer_counter.h
@@ -4,24 +4,35 @@
 #include "hash/rolling_window.h"
 #include "kmer_counts_manager.h"
 
-void countKMersForContiguousSeq(int k,
-                                int begin,
-                                int end,
-                                RollingWindow& rollingWindow,
-                                KMerCountsManager& kmerCountsManager,
-                                bool isPositionalKMer) {
+template<class input_vector_t, class input_elem_t, class encoded_elem_t>
+inline void updateKMerCounts(RollingWindow<input_vector_t, input_elem_t, encoded_elem_t>& rollingWindow,
+                             KMerCountsManager& kmerCountsManager,
+                             bool isPositionalKMer) {
+  kmerCountsManager.add(
+    isPositionalKMer ? rollingWindow.getWindowedPositionedHashes()
+                     : rollingWindow.getWindowedHashes(),
+    rollingWindow.currentBeginIndex()
+  );
+}
+
+template<class input_vector_t, class input_elem_t, class encoded_elem_t>
+inline void countKMersForContiguousSeq(int k,
+                                       int begin,
+                                       int end,
+                                       RollingWindow<input_vector_t, input_elem_t, encoded_elem_t>& rollingWindow,
+                                       KMerCountsManager& kmerCountsManager,
+                                       bool isPositionalKMer) {
   rollingWindow.resetIndex(begin);
   for(int i = 0; i < k; ++i) {
     rollingWindow.append();
   }
-  for(int beginPosition = begin; beginPosition <= end; ++beginPosition) {
-    kmerCountsManager.add(
-      isPositionalKMer ? rollingWindow.getWindowedPositionedHashes()
-                       : rollingWindow.getWindowedHashes(),
-      beginPosition
-    );
+  for(int beginPosition = begin; beginPosition < end; ++beginPosition) {
+    updateKMerCounts(rollingWindow, kmerCountsManager, isPositionalKMer);
     rollingWindow.moveWindowRight();
   }
+  updateKMerCounts(rollingWindow, kmerCountsManager, isPositionalKMer);
 }
+
+
 
 #endif //KMER_COUNTER_H
