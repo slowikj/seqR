d84bc2a73623468610ac6e8861a510a2e0eece07
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-03-08 19:42:15 +0100

    dictionary: Add const_iterator

--- src/dictionary.h
+++ src/dictionary.h
@@ -8,52 +8,57 @@
 #include <unordered_map>
 #include <functional>
 
+template<class ITER_V, class INNER_CONTAINER_IT_T>
+class iterator_t {
+public:
+  // iterator_traits
+  using value_type = ITER_V;
+  using pointer = ITER_V *;
+  using reference = ITER_V &;
+  using iterator_category = std::forward_iterator_tag;
+  using difference_type = void;
+  
+  explicit iterator_t(INNER_CONTAINER_IT_T it) : container_iterator_(it) {}
+  
+  iterator_t<ITER_V, INNER_CONTAINER_IT_T>& operator++() {
+    ++this->container_iterator_;
+    return *this;
+  }
+  
+  iterator_t<ITER_V, INNER_CONTAINER_IT_T> operator++(int) {
+    auto old = iterator(this->container_iterator_);
+    ++this->container_iterator_;
+    return old;
+  }
+  
+  value_type operator*() {
+    return *(this->container_iterator_);
+  }
+  
+  iterator_t<ITER_V, INNER_CONTAINER_IT_T> operator->() {
+    return (this->container_iterator_);
+  }
+  
+  bool operator==(const iterator_t<ITER_V, INNER_CONTAINER_IT_T>& other) const {
+    return this->container_iterator_ == other.container_iterator_;
+  }
+  
+  bool operator!=(const iterator_t<ITER_V, INNER_CONTAINER_IT_T>& other) const {
+    return !((*this) == other);
+  }
+  
+private:
+  INNER_CONTAINER_IT_T container_iterator_;
+  
+};
+
 template<class K, class V, class Hash=std::hash<K>>
 class Dictionary {
+
 public:
   
-  class iterator {
-  public:
-    // iterator_traits
-    using value_type = std::pair<const K&, V>;
-    using pointer = std::pair<const K&, V> *;
-    using reference = std::pair<const K&, V> &;
-    using iterator_category = std::forward_iterator_tag;
-    using difference_type = void;
-    
-    explicit iterator(typename std::unordered_map<K, V, Hash>::iterator it) : container_iterator_(it) {}
-    
-    iterator& operator++() {
-      ++this->container_iterator_;
-      return *this;
-    }
-    
-    iterator operator++(int) {
-      auto old = iterator(this->container_iterator_);
-      ++this->container_iterator_;
-      return old;
-    }
-    
-    value_type operator*() {
-      return *(this->container_iterator_);
-    }
-    
-    iterator operator->() {
-      return (this->container_iterator_);
-    }
-    
-    bool operator==(const iterator& other) {
-      return this->container_iterator_ == other.container_iterator_;
-    }
-    
-    bool operator!=(const iterator& other) {
-      return !((*this) == other);
-    }
-    
-  private:
-    typename std::unordered_map<K, V, Hash>::iterator container_iterator_;
-    
-  };
+  using iterator = iterator_t<std::pair<const K&, V>, typename std::unordered_map<K, V, Hash>::iterator>;
+  using const_iterator = iterator_t<const std::pair<const K&, V>, typename std::unordered_map<K, V, Hash>::const_iterator>;
   
   V &operator[](const K &key);
   
@@ -65,7 +70,11 @@ public:
   
   iterator begin();
   
+  const_iterator begin() const noexcept;
+  
   iterator end();
+  
+  const_iterator end() const noexcept;
   
   Dictionary() = default;
   
@@ -116,8 +125,20 @@ inline
 
 template<class K, class V, class Hash>
 inline
+  typename Dictionary<K, V, Hash>::const_iterator Dictionary<K, V, Hash>::begin() const noexcept {
+    return const_iterator(this->inner_map_.begin());
+  }
+
+template<class K, class V, class Hash>
+inline
   typename Dictionary<K, V, Hash>::iterator Dictionary<K, V, Hash>::end() {
     return iterator(this->inner_map_.end());
+  }
+
+template<class K, class V, class Hash>
+inline
+  typename Dictionary<K, V, Hash>::const_iterator Dictionary<K, V, Hash>::end() const noexcept {
+    return const_iterator(this->inner_map_.end());
   }
 
 template<class K, class V, class Hash>
