59024fd18167d9183a50abf32831621ab7c75e28
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-03-09 03:18:28 +0100

    Clean up: move creating k-mer names for one sequence to the proxy file

--- src/count_kmers_proxy.cpp
+++ src/count_kmers_proxy.cpp
@@ -5,7 +5,29 @@
 #include "kmer_counter.h"
 #include <iostream>
 #include <algorithm>
+#include <functional>
 #include "kmer_strings_creator.h"
+
+Rcpp::StringVector getKMerNames(
+    const Dictionary<std::vector<int>, KMerHashInfo, vector_int_hasher>& kmerCountsDictionary,
+    const Rcpp::StringVector& sequence,
+    const Rcpp::IntegerVector& gaps,
+    bool isPositionalKMer) {
+  KMerCreator kmerCreator(sequence, gaps, ".");
+  std::function<std::string(int)> createKMer = isPositionalKMer ?
+  static_cast<std::function<std::string(int)>>([&kmerCreator](int begin) {
+    return kmerCreator.getPositional(begin, "_");
+  }) :
+    static_cast<std::function<std::string(int)>>([&kmerCreator](int begin) {
+      return kmerCreator.get(begin);
+    });
+  Rcpp::StringVector res(kmerCountsDictionary.size());
+  int resIndex = 0;
+  for(const auto& kmerDictElem: kmerCountsDictionary) {
+    res[resIndex++] = createKMer(kmerDictElem.second.seqStartPosition);
+  }
+  return res;
+}
 
 //' @export
 // [[Rcpp::export]]
--- src/kmer_strings_creator.cpp
+++ src/kmer_strings_creator.cpp
@@ -1,6 +1,5 @@
 #include "kmer_strings_creator.h"
-#include<utility>
-#include<functional>
+#include <utility>
 
 std::size_t KMerCreator::getTotalSize(int begin, int separatorLength) const {
   return this->sequence[begin].size() + std::accumulate(
@@ -30,23 +29,3 @@ std::string KMerCreator::getPositional(int position,
   return std::to_string(position + 1) + positionSeparator + withoutPositionString;
 }
 
-Rcpp::StringVector getKMerNames(
-    const Dictionary<std::vector<int>, KMerHashInfo, vector_int_hasher>& kmerCountsDictionary,
-    const Rcpp::StringVector& sequence,
-    const Rcpp::IntegerVector& gaps,
-    bool isPositionalKMer) {
-  KMerCreator kmerCreator(sequence, gaps, ".");
-  std::function<std::string(int)> createKMer = isPositionalKMer ?
-    static_cast<std::function<std::string(int)>>([&kmerCreator](int begin) {
-      return kmerCreator.getPositional(begin, "_");
-    }) :
-    static_cast<std::function<std::string(int)>>([&kmerCreator](int begin) {
-      return kmerCreator.get(begin);
-    });
-  Rcpp::StringVector res(kmerCountsDictionary.size());
-  int resIndex = 0;
-  for(const auto& kmerDictElem: kmerCountsDictionary) {
-    res[resIndex++] = createKMer(kmerDictElem.second.seqStartPosition);
-  }
-  return res;
-}
