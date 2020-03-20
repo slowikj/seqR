16a6a2bd2c1104e169d14bc485de06f5d7e7f6b1
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-03-20 21:52:12 +0100

    alphabet_encoder: add notAllowedEncodingNum + index start from 2

--- src/alphabet_encoder.h
+++ src/alphabet_encoder.h
@@ -12,9 +12,11 @@ template<class input_elem_t, class internal_elem_t, class encoded_elem_t>
 class AlphabetEncoding {
 public:
   AlphabetEncoding(std::function<internal_elem_t(const input_elem_t&)> inputToInternalItemConverter,
-                   Dictionary<internal_elem_t, encoded_elem_t>&& internalToEncoded):
+                   Dictionary<internal_elem_t, encoded_elem_t>&& internalToEncoded,
+                   encoded_elem_t notAllowedEncodingNum):
     internalToEncoded(std::move(internalToEncoded)),
-    inputToInternalItemConverter(inputToInternalItemConverter) {
+    inputToInternalItemConverter(inputToInternalItemConverter),
+    notAllowedEncodingNum(notAllowedEncodingNum) {
   }
   
   AlphabetEncoding() = default;
@@ -35,22 +37,25 @@ public:
     return internalToEncoded.isPresent(inputToInternalItemConverter(inputElem));
   }
   
+  encoded_elem_t getNotAllowedEncodingNum() const {
+    return this->notAllowedEncodingNum;
+  }
+  
   std::size_t alphabetSize() const {
     return internalToEncoded.size();
   }
   
 private:
   Dictionary<internal_elem_t, encoded_elem_t> internalToEncoded;
-  
   std::function<internal_elem_t(const input_elem_t&)> inputToInternalItemConverter;
+  encoded_elem_t notAllowedEncodingNum;
 };
 
 template<class input_t, class input_elem_t, class internal_elem_t, class encoded_elem_t>
 AlphabetEncoding<input_elem_t, internal_elem_t, encoded_elem_t> getAlphabetEncoding(
     input_t& input,
-    std::function<internal_elem_t(const input_elem_t&)> inputToInternalItemConverter,
-    encoded_elem_t startEncodingNum = 1) {
-  encoded_elem_t currentNum = startEncodingNum;
+    std::function<internal_elem_t(const input_elem_t&)> inputToInternalItemConverter) {
+  encoded_elem_t currentNum = 2;
   Dictionary<internal_elem_t, encoded_elem_t> internalToEncoded;
   for(const auto& inputElem: input) {
     internal_elem_t internalElem = inputToInternalItemConverter(inputElem);
@@ -60,7 +65,8 @@ AlphabetEncoding<input_elem_t, internal_elem_t, encoded_elem_t> getAlphabetEncod
   }
   return AlphabetEncoding<input_elem_t, internal_elem_t, encoded_elem_t>(
       inputToInternalItemConverter,
-      std::move(internalToEncoded));
+      std::move(internalToEncoded),
+      1);
 }
 
 #endif //ALPHABET_ENCODER_H
