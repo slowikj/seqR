bf119d06e0b1be7b0af51a0ddc143a33f92a91e8
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-03-24 01:27:56 +0100

    Add tests for integer matrices

--- tests/testthat/test_count_kmers_single_seq.R
+++ tests/testthat/test_count_kmers_single_seq.R
@@ -1,19 +1,33 @@
 library(testthat)
 source("utils.R")
 
-invoke_test_string <- function(expected_res, alphabet, sequence, k, positionalKMers) {
+invoke_test <- function(fun, expected_res, alphabet, sequence, k, positionalKMers) {
   sequenceMatrix <- to_matrix(sequence)
   expected_res <- to_matrix(expected_res)
   
-  res <- seqR::count_kmers_string(alphabet=alphabet,
-                                  sequenceMatrix = sequenceMatrix,
-                                  k=k,
-                                  positionalKMers = positionalKMers)
+  res <- fun(alphabet=alphabet,
+             sequenceMatrix = sequenceMatrix,
+             k=k,
+             positionalKMers = positionalKMers)
   
   expect_matrices_equal(res, expected_res)
 }
 
-test_that("count non positional 2-mers", {
+invoke_test_string <- function(...) {
+  invoke_test(seqR::count_kmers_string, ...)
+}
+
+invoke_test_integer <- function(...) {
+  invoke_test(seqR::count_kmers_integer, ...)
+}
+
+invoke_test_numeric <- function(...) {
+  invoke_test(seqR::count_kmers_numeric, ...)
+}
+
+# STRING
+
+test_that("(string) count non positional 2-mers", {
   invoke_test_string(expected_res = c("a.a"=1, "a.b"=2, "b.a"=2),
                      alphabet=c("a", "b"),
                      sequence=c("a", "b", "a", "b", "a", "a"),
@@ -21,7 +35,7 @@ test_that("count non positional 2-mers", {
                      positionalKMers=FALSE)
 })
 
-test_that("count positional 2-mers", {
+test_that("(string) count positional 2-mers", {
   invoke_test_string(expected_res = c("1_a.b"=1, "2_b.a"=1, "3_a.b"=1, "4_b.a"=1, "5_a.a"=1),
                      alphabet=c("a", "b"),
                      sequence=c("a", "b", "a", "b", "a", "a"),
@@ -29,7 +43,7 @@ test_that("count positional 2-mers", {
                      positionalKMers=TRUE)
 })
 
-test_that("count non positional 1-mers", {
+test_that("(string) count non positional 1-mers", {
   invoke_test_string(expected_res=c("a"=3, "b"=2),
                      alphabet=c("a", "b"),
                      sequence=c("a", "a", "b", "a", "b"),
@@ -37,7 +51,7 @@ test_that("count non positional 1-mers", {
                      positionalKMers=FALSE)
 })
 
-test_that("count positional 1-mers", {
+test_that("(string) count positional 1-mers", {
   invoke_test_string(expected_res=c("1_a"=1, "2_a"=1, "3_b"=1, "4_a"=1, "5_b"=1),
                      alphabet=c("a", "b"),
                      sequence=c("a", "a", "b", "a", "b"),
@@ -45,7 +59,7 @@ test_that("count positional 1-mers", {
                      positionalKMers=TRUE)
 })
 
-test_that("count non positional 1-mers if some sequence items are not allowed", {
+test_that("(string) count non positional 1-mers if some sequence items are not allowed", {
   invoke_test_string(expected_res=c("a"=3),
                      alphabet=c("a"),
                      sequence=c("a", "a", "b", "a", "b"),
@@ -53,7 +67,7 @@ test_that("count non positional 1-mers if some sequence items are not allowed", 
                      positionalKMers=FALSE)
 })
 
-test_that("count positional 1-mers if some sequence items are not allowed", {
+test_that("(string) count positional 1-mers if some sequence items are not allowed", {
   invoke_test_string(expected_res=c("1_a"=1, "2_a"=1, "4_a"=1),
                      alphabet=c("a"),
                      sequence=c("a", "a", "b", "a", "b"),
@@ -61,7 +75,7 @@ test_that("count positional 1-mers if some sequence items are not allowed", {
                      positionalKMers=TRUE)
 })
 
-test_that("count non positional 3-mers", {
+test_that("(string) count non positional 3-mers", {
   invoke_test_string(expected_res=c("a.a.b"=2, "a.b.c"=2, "b.c.a"=2, "c.a.a"=1),
                      alphabet=c("a", "b", "c"),
                      sequence=c("a", "a", "b", "c", "a", "a", "b", "c", "a"),
@@ -69,10 +83,20 @@ test_that("count non positional 3-mers", {
                      positionalKMers=FALSE)
 })
 
-test_that("count positional 3-mers", {
+test_that("(string) count positional 3-mers", {
   invoke_test_string(expected_res=c("1_a.a.a"=1, "2_a.a.b"=1, "3_a.b.a"=1, "4_b.a.a"=1, "5_a.a.a"=1),
                      alphabet=c("a", "b"),
                      sequence=c("a", "a", "a", "b", "a", "a", "a"),
                      k=3,
                      positionalKMers=TRUE)
 })
+
+# INTEGER
+test_that("(integer) count non positional 2-mers with not allowed item", {
+  invoke_test_integer(expected_res=c("1.0"=2, "1.1"=3, "0.1"=2),
+                      alphabet=c(0, 1),
+                      sequence=c(1,0,1,1,0,1,1,2,1,1),
+                      k=2,
+                      positionalKMers=FALSE)
+})
+
--- tests/testthat/test_gapped_kmers.R
+++ tests/testthat/test_gapped_kmers.R
@@ -1,12 +1,26 @@
 library(testthat)
 source("utils.R")
 
-invoke_test_strings <- function(expected_res, ...) {
-  res <- seqR::count_gapped_kmers_string(...)
+invoke_test <- function(fun, expected_res, ...) {
+  res <- fun(...)
   expect_matrices_equal(expected_res, res)
 }
 
-test_that("test one sequence with gaps (1) not positional", {
+invoke_test_strings <- function(...) {
+  invoke_test(seqR::count_gapped_kmers_string, ...)
+}
+
+invoke_test_integer <- function(...) {
+  invoke_test(seqR::count_gapped_kmers_integer, ...)
+}
+
+invoke_test_numeric <- function(...) {
+  invoke_test(seqR::count_gapped_kmers_numeric, ...)
+}
+
+# STRING
+
+test_that("(string)test one sequence with gaps (1) not positional", {
   m <- matrix(c("a", "a", "a", "b", "a", "b", "a" ),
               nrow=1)
   
@@ -17,7 +31,7 @@ test_that("test one sequence with gaps (1) not positional", {
                       positionalKMers=FALSE)
 })
 
-test_that("test one sequence with gaps (1) positional", {
+test_that("(string)test one sequence with gaps (1) positional", {
   m <- matrix(c("a", "a", "a", "b", "a", "b", "a" ),
               nrow=1)
   
@@ -28,7 +42,7 @@ test_that("test one sequence with gaps (1) positional", {
                       positionalKMers=TRUE)
 })
 
-test_that("test one sequence with gapps (1,0) not positional", {
+test_that("(string)test one sequence with gapps (1,0) not positional", {
   m <- matrix(c("a", "a", "a", "b", "a", "b", "a" ),
               nrow=1)
   
@@ -39,7 +53,7 @@ test_that("test one sequence with gapps (1,0) not positional", {
                       positionalKMers=FALSE)
 })
 
-test_that("test 2 sequences with gaps (1,1) positional; some items are not from alphabet", {
+test_that("(string)test 2 sequences with gaps (1,1) positional; some items are not from alphabet", {
   seqMatrix <- matrix(c(
     "a", "b", "c", "c", "as", "b", "c", "a", "a", "b", "a",
     "b", "b", "c", "c", "a",  "a", "b", "c", "a", "b", "a"
@@ -50,9 +64,28 @@ test_that("test 2 sequences with gaps (1,1) positional; some items are not from 
   ), nrow=2, byrow=TRUE)
   colnames(expectedRes) <- c("6_b.a.b", "7_b.a.a", "5_a.b.a")
   
-  invoke_test_strings(expected_res <- expectedRes,
+  invoke_test_strings(expected_res=expectedRes,
                       alphabet=c("a", "b"),
                       sequenceMatrix=seqMatrix,
                       gaps=c(1,1),
                       positionalKMers=TRUE)
+})
+
+# INTEGER
+
+test_that("(integer) test 2 sequences with gaps (1,1) non positional; some items are not from alphabet", {
+  seqMatrix <- matrix(c(
+    1, 2, 0, 1, 1, 1, 2, 1, 0, 1,
+    1, 1, 1, 0, 0, 0, 0, 2, 2, 1
+  ), nrow=2, byrow=TRUE)
+  expected_res <- matrix(c(
+    1, 2, 0, 0,
+    0, 0, 1, 2 
+  ), nrow=2, byrow=TRUE)
+  colnames(expected_res) <- c("1.0.1", "1.1.1", "1.1.0", "1.0.0")
+  invoke_test_integer(expected_res=expected_res,
+                      alphabet=c(0,1),
+                      sequenceMatrix=seqMatrix,
+                      gaps=c(1,1),
+                      positionalKMers=FALSE)
 })
