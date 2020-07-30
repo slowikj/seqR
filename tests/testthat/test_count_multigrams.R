cde1869479616c7cbca6d7e13d5ec6a7efecc16c
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-07-30 14:58:45 +0200

    Add count_multimers

---
+++ tests/testthat/test_count_multigrams.R
@@ -0,0 +1,56 @@
+library(testthat)
+source("utils.R")
+
+invoke_test <- function(expected_res,
+                        sequences,
+                        k_vector,
+                        alphabet,
+                        kmer_gaps_list,
+                        positional_vector,
+                        with_kmer_counts, kmer_dictionary_name, batch_size) {
+  triplet_matrix_res <- seqR::count_multimers(sequences, k_vector, alphabet, positional_vector,
+                                              kmer_gaps_list,
+                                              with_kmer_counts, kmer_dictionary_name, batch_size)
+  expect_matrices_equal(expected_res, as.matrix(triplet_matrix_res))
+}
+
+test_that("count 2-mers and 3-mers for tidysq (AC){100}", {
+  expected_res <- matrix(c(
+    100, 99, 99, 99
+  ), nrow=1, byrow=TRUE)
+  colnames(expected_res) <- c("A.C_0", "C.A_0", "A.C.A_0.0", "C.A.C_0.0")
+  
+  sq <- tidysq::as.sq(strrep("AC", 100))
+  
+  invoke_test(expected_res=expected_res,
+              sequences=sq,
+              k_vector=c(2, 3),
+              alphabet=c("A", "C"),
+              positional_vector=rep(FALSE, 2),
+              kmer_gaps_list=list(c(), c()),
+              with_kmer_counts = TRUE,
+              kmer_dictionary_name="unordered_map",
+              batch_size=200
+  )
+})
+
+test_that("count 2-mers and 3-mers for 2 sequences: (AC){100}, (AD){10}", {
+  expected_res <- matrix(c(
+    100, 99, 99, 99, 0, 0, 0, 0,
+    0,    0,  0,  0, 10, 9, 9, 9
+  ), nrow=2, byrow=TRUE)
+  colnames(expected_res) <- c("A.C_0", "C.A_0", "A.C.A_0.0", "C.A.C_0.0", "A.D_0", "D.A_0", "A.D.A_0.0", "D.A.D_0.0")
+  
+  sq <- tidysq::as.sq(c(strrep("AC", 100), strrep("AD", 10)))
+  
+  invoke_test(expected_res=expected_res,
+              sequences=sq,
+              k_vector=c(2, 3),
+              alphabet=c("A", "C", "D"),
+              positional_vector=rep(FALSE, 2),
+              kmer_gaps_list=list(c(), c()),
+              with_kmer_counts = TRUE,
+              kmer_dictionary_name="unordered_map",
+              batch_size=200
+  )
+})
