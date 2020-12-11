860feae543957e2aebd7bc54dc6fc36875e639ac
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-12-11 18:28:04 +0100

    Refactor: supported_dict_names.h: merge nested namespaces

--- src/dictionary/supported_dict_names.h
+++ src/dictionary/supported_dict_names.h
@@ -3,15 +3,12 @@
 
 #include <string>
 
-namespace dictionary {
-
-    namespace names {
-        const std::string UNORDERED_MAP_NAME = "unordered_map";
-        const std::string LINEAR_LIST_NAME = "linear_list";
-        const std::string ORDERED_MAP_NAME = "ordered_map";
-        const std::string EMILIB_HASH_MAP_WRAPPER = "emilib_hash_map";
-        const std::string MARTINUS_ROBIN_HOOD_DICTIONARY = "robin_hood_map";
-    }
+namespace dictionary::names {
+    const std::string UNORDERED_MAP_NAME = "unordered_map";
+    const std::string LINEAR_LIST_NAME = "linear_list";
+    const std::string ORDERED_MAP_NAME = "ordered_map";
+    const std::string EMILIB_HASH_MAP_WRAPPER = "emilib_hash_map";
+    const std::string MARTINUS_ROBIN_HOOD_DICTIONARY = "robin_hood_map";
 }
 
 
