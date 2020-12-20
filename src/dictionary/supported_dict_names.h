dac857bae37c67665a3b1eae0df2b9ae9c926ad0
Jadwiga Słowik <jadwis.slowik@gmail.com>
2020-12-20 18:39:47 +0100

    Add a new, third-party hash map (flat map)

--- src/dictionary/supported_dict_names.h
+++ src/dictionary/supported_dict_names.h
@@ -9,6 +9,7 @@ namespace dictionary::names {
     inline const std::string STL_ORDERED_MAP = "ordered_map";
     inline const std::string EMILIB_HASH_MAP = "emilib_hash_map";
     inline const std::string MARTINUS_ROBIN_HOOD_DICTIONARY = "robin_hood_map";
+    inline const std::string FLAT_HASHMAP = "flat_hash_map";
 }
 
 
