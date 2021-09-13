1f5c72a182bdde6f4a71cb87a9acd677515d6112
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2021-09-13 21:54:40 +0200

    improved examples

--- R/package.R
+++ R/package.R
@@ -12,7 +12,22 @@
 #' @aliases seqR
 #' 
 #' @name seqR-package
-#' 
 #' @useDynLib seqR, .registration=TRUE
 #' @importFrom Rcpp sourceCpp
+#' @examples
+#' # Load exemplary sequences
+#' data(CsgA)
+#' 
+#' # Counting 1-mers (amino acid composition)
+#' count_kmers(
+#'     CsgA,
+#'     k = 1,
+#'     batch_size = 1)   
+#' 
+#' 
+#' # Counting 1-mers and 2-mers
+#' count_multimers(
+#'     CsgA,
+#'     k_vector = c(1, 2),
+#'     batch_size = 1)
 NULL
