45472722fa90a79bbb1d9a4ab4c7a957d9969f9a
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2021-09-13 21:36:34 +0200

    Fixed dataset and removed unnecessary fields

--- R/CsgA.R
+++ R/CsgA.R
@@ -1,8 +1,8 @@
 #' @name CsgA
 #' @title CsgA data set
-#' @description 24 reviewed sequences of CsgA proteins obtained from UniProt on 13-09-2021.
+#' @description 5 reviewed sequences of CsgA proteins obtained from UniProt on 13-09-2021.
 #' @docType data
 #' @usage CsgA
-#' @format a list of 24 proteins.
+#' @format a list of 5 proteins.
 #' @keywords datasets
 NULL
