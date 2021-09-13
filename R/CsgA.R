241d3bad36615d1b6fea6b08dd72a17e034e8652
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2021-09-13 21:25:41 +0200

    added CsgA example data

---
+++ R/CsgA.R
@@ -0,0 +1,8 @@
+#' @name CsgA
+#' @title CsgA data set
+#' @description 24 reviewed sequences of CsgA proteins obtained from UniProt on 13-09-2021.
+#' @docType data
+#' @usage CsgA
+#' @format a list of 24 proteins.
+#' @keywords datasets
+NULL
